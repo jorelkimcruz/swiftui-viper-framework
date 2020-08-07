d078dd52285a3974affd2e5748506be863226ccb
jorelkim <jorelkim@salarium.com>
2020-08-07 13:29:57 +0800

    Initial Commit

---
+++ viper-swift-ui-portfolio/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  viper-swift-ui-portfolio
+//
+//  Created by MAC HOME on 8/7/20.
+//  Copyright © 2020 JORELCRUZ. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ viper-swift-ui-portfolioTests/viper_swift_ui_portfolioTests.swift
@@ -0,0 +1,34 @@
+//
+//  viper_swift_ui_portfolioTests.swift
+//  viper-swift-ui-portfolioTests
+//
+//  Created by MAC HOME on 8/7/20.
+//  Copyright © 2020 JORELCRUZ. All rights reserved.
+//
+
+import XCTest
+@testable import viper_swift_ui_portfolio
+
+class viper_swift_ui_portfolioTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
